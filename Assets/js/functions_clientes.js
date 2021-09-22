720c308a22038f2acf99f9ef349fb53305262a52
jbonillaz <damilan1991@gmail.com>
2021-09-22 16:35:04 -0500

    Se habilito la accion ver informacion del cliente.

--- Assets/js/functions_clientes.js
+++ Assets/js/functions_clientes.js
@@ -1,4 +1,58 @@
+let tableClientes;
+// let rowTable = "";
+let divLoading = document.querySelector("#divLoading");
 document.addEventListener('DOMContentLoaded', function() {
+
+    // DataTable
+    tableClientes = $('#tableClientes').dataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        },
+        "ajax": {
+            "url": " " + base_url + "/Clientes/getClientes",
+            "dataSrc": ""
+        },
+        "columns": [
+            { "data": "idpersona" },
+            { "data": "identificacion" },
+            { "data": "nombres" },
+            { "data": "apellidos" },
+            { "data": "email_user" },
+            { "data": "telefono" },
+            { "data": "options" }
+        ],
+        'dom': 'lBfrtip',
+        'buttons': [{
+            "extend": "copyHtml5",
+            "text": "<i class='far fa-copy'></i> Copiar",
+            "titleAttr": "Copiar",
+            "className": "btn btn-secondary"
+        }, {
+            "extend": "excelHtml5",
+            "text": "<i class='fas fa-file-excel'></i> Excel",
+            "titleAttr": "Esportar a Excel",
+            "className": "btn btn-success"
+        }, {
+            "extend": "pdfHtml5",
+            "text": "<i class='fas fa-file-pdf'></i> PDF",
+            "titleAttr": "Esportar a PDF",
+            "className": "btn btn-danger"
+        }, {
+            "extend": "csvHtml5",
+            "text": "<i class='fas fa-file-csv'></i> CSV",
+            "titleAttr": "Esportar a CSV",
+            "className": "btn btn-info"
+        }],
+        "resonsieve": "true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order": [
+            [0, "desc"]
+        ]
+    });
+
 
     if (document.querySelector("#formCliente")) {
         var formCliente = document.querySelector("#formCliente");
@@ -43,6 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         $('#modalformCliente').modal("hide");
                         formCliente.reset();
                         swal("Usuarios", objData.msg, "success");
+                        tableClientes.api().ajax.reload();
                     } else {
                         swal("Error", objData.msg, "error");
                     }
@@ -55,7 +110,62 @@ document.addEventListener('DOMContentLoaded', function() {
 
 }, false);
 
+function fntViewInfo(idpersona) {
+    let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    let ajaxUrl = base_url + '/Clientes/getCliente/' + idpersona;
+    request.open("GET", ajaxUrl, true);
+    request.send();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            let objData = JSON.parse(request.responseText);
+            if (objData.status) {
+                document.querySelector("#celIdentificacion").innerHTML = objData.data.identificacion;
+                document.querySelector("#celNombre").innerHTML = objData.data.nombres;
+                document.querySelector("#celApellido").innerHTML = objData.data.apellidos;
+                document.querySelector("#celTelefono").innerHTML = objData.data.telefono;
+                document.querySelector("#celEmail").innerHTML = objData.data.email_user;
+                document.querySelector("#celIde").innerHTML = objData.data.nit;
+                document.querySelector("#celNomFiscal").innerHTML = objData.data.nombrefiscal;
+                document.querySelector("#celDirFiscal").innerHTML = objData.data.direccionfiscal;
+                document.querySelector("#celFechaRegistro").innerHTML = objData.data.fechaRegistro;
+                $('#modalViewCliente').modal('show');
+            } else {
+                swal("Error", objData.msg, "error");
+            }
+        }
+    }
+}
 
+// Editar cliente
+function fntEditInfo(element, idpersona) {
+    rowTable = element.parentNode.parentNode.parentNode;
+    document.querySelector('#titleModal').innerHTML = "Actualizar Cliente";
+    document.querySelector('.modal-header').classList.replace("headerRegister", "headerUpdate");
+    document.querySelector('#btnActionForm').classList.replace("btn-primary", "btn-info");
+    document.querySelector('#btnText').innerHTML = "Actualizar";
+    let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    let ajaxUrl = base_url + '/Clientes/getCliente/' + idpersona;
+    request.open("GET", ajaxUrl, true);
+    request.send();
+    request.onreadystatechange = function() {
+
+        if (request.readyState == 4 && request.status == 200) {
+            let objData = JSON.parse(request.responseText);
+            if (objData.status) {
+                document.querySelector("#idUsuario").value = objData.data.idpersona;
+                document.querySelector("#txtIdentificacion").value = objData.data.identificacion;
+                document.querySelector("#txtNombre").value = objData.data.nombres;
+                document.querySelector("#txtApellido").value = objData.data.apellidos;
+                document.querySelector("#txtTelefono").value = objData.data.telefono;
+                document.querySelector("#txtEmail").value = objData.data.email_user;
+                document.querySelector("#txtNit").value = objData.data.nit;
+                document.querySelector("#txtNombreFiscal").value = objData.data.nombrefiscal;
+                document.querySelector("#txtDirFiscal").value = objData.data.direccionfiscal;
+            }
+        }
+        $('#modalFormCliente').modal('show');
+    }
+}
 
 
 function openModal() {
